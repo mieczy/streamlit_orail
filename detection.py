828d01edc9fb5aa16d308c5ce012580fdbc3ee8f
mieczy <mieczy@naver.com>
2023-10-13 14:22:46 +0900

    chore: 파일 위치 변경

--- detection.py
+++ detection.py
@@ -1,24 +1,28 @@
 import torch
 import torchvision.transforms as transforms
 from torchvision.models import densenet121
+import pandas as pd
 
-from os.path import isfile
+from functools import lru_cache
+
 from PIL import Image
 
 from typing import Final
 
+import logging
+
 
 class OrailModel:
     DIR: Final = './ml/'
-    CLASS_NAMES: Final = ['NORMAL', 'AVG', 'MULTITREND', 'HUNTING', 'DRIFT']
 
-    def __init__(self):
+    def __init__(self, name):
         self.model = densenet121(num_classes=5) 
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model.load_state_dict(torch.load(self.DIR+'Densenet121.pth', map_location=torch.device('cpu')))
+        self.model.load_state_dict(torch.load(self.DIR+name, map_location=torch.device('cpu')))
         self.model.eval()
         self.model.to(self.device)
         self.model.eval()
+
 
     def convert_image(self, file):
         with (Image.open(file) as image):
@@ -37,6 +41,7 @@ class OrailModel:
 
         return input_image
     
+
     def inference_single_image(self, input_image):
         with torch.no_grad():
             input_image = input_image.to(self.device)
@@ -45,28 +50,30 @@ class OrailModel:
             probabilities = torch.softmax(y_pred, dim=1)
 
         predicted_label = predicted.item()
-        label_probabilities = {self.CLASS_NAMES[i]: prob.item() for i, prob in enumerate(probabilities[0])}
-
+        label_probabilities = [prob.item()*100 for i, prob in enumerate(probabilities[0])]
+        
         return predicted_label, label_probabilities
-    
-    def get_result(self, predicted_label, label_probabilities):
-        # ft. optional label_probabilities 
-        # ft. score/confident level calculate
-        if predicted_label == 0:
-            result = 'Normal Type'
-            odd_image = 'No'
-        else :
-            result = 'Abnormal Type '  + str(predicted_label) + ': ' + self.CLASS_NAMES[predicted_label]
-            odd_image = 'Yes'
-
-        return result, odd_image
     
         
     def detect(self, image_file):
         try:
             input_image = self.convert_image(image_file)
             predicted_label, label_probabilities = self.inference_single_image(input_image)
+            
+            p = label_probabilities[predicted_label]
+            if p <= 75:
+                is_odd = 'Yes (Score1)'
+            elif p <= 85:
+                is_odd = 'Yes (Score2)'
+            else:
+                is_odd = 'No'
+            
+            return predicted_label, label_probabilities, is_odd
         
-            return self.get_result(predicted_label, label_probabilities)
         except Exception  as e:
+            logging.error(e)
             raise Exception()
+        
+@lru_cache
+def get_model(name: str):
+    return OrailModel(name)
